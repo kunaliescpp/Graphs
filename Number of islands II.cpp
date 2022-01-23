78c7bedd6fdb98cfeb6f6b8139808fd2a8e22df9
kunal_cpp <kunaliesc@gmail.com>
2022-01-23 19:04:11 +0530

    Update Number of islands II.cpp

--- Number of islands II.cpp
+++ Number of islands II.cpp
@@ -1,119 +1,114 @@
 /* 
-Problem Link: https://www.lintcode.com/problem/804/
+Problem Link: https://www.lintcode.com/problem/434/
 
-Number of Distinct Islands
+Number of Islands II
 
-Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally
-(horizontal or vertical). You may assume all four edges of the grid are surrounded by water. Count the number of distinct 
-islands. An island is considered to be the same as another if and only if one island has the same shape as another island 
-(and not rotated or reflected).
-
-Note - The length of each dimension in the given grid does not exceed 50.
+Given a n,m which means the row and column of the 2D matrix and an array of pair A( size k). Originally, the 2D matrix is all 0 
+which means there is only sea in the matrix. The list pair has k operator and each operator has two integer A[i].x, A[i].y means
+that you can change the grid matrix[A[i].x][A[i].y] from sea to island. Return how many island are there in the matrix after each 
+operator.You need to return an array of size K. 0 is represented as the sea, 1 is represented as the island. If two 1 is adjacent,
+we consider them in the same island. We only consider up/down/left/right adjacent.
 
 Examples: 
 Example 1:
-Input: [[1,1,0,0,1],[1,0,0,0,0],[1,1,0,0,1],[0,1,0,1,1]]
-Output: 3
-Explanation: The island is look like this:
-              11000
-              10000
-              00001
-              00011
-Notice that 11  and  1 are considered same island shapes. Because if we make a 180 degrees clockwise rotation on the first 
-            1       11 
-island, then two islands will have the same shapes.
-            
+Input: n = 4, m = 5, A = [[1,1],[0,1],[3,3],[3,4]]
+Output: [1,1,2,2]
+Explanation:
+0.  00000
+    00000
+    00000
+    00000
+1.  00000
+    01000
+    00000
+    00000
+2.  01000
+    01000
+    00000
+    00000
+3.  01000
+    01000
+    00000
+    00010
+4.  01000
+    01000
+    00000
+    00011
+    
 Example 2:
-Input: [[1,1,0,0,0],[1,1,0,0,0],[0,0,0,1,1],[0,0,0,1,1]]
-Output: 1
-*/
+Input: n = 3, m = 3, A = [[0,0],[0,1],[2,2],[2,1]]
+Output: [1,1,2,2]
+
+/**
+ * Definition for a point.
+ * struct Point {
+ *     int x;
+ *     int y;
+ *     Point() : x(0), y(0) {}
+ *     Point(int a, int b) : x(a), y(b) {}
+ * };
+ */
 
 class Solution {
 public:
+   
+    int find(int x, vector<int>& par){
+        if(par[x] == x) return x;
 
-    static bool mycmp(pair<int, int>& a, pair<int, int>& b){
-        
-        if(a.second > b.second) return a.first > b.first;
-
-    return a.first < b.first;
-    }
-
-    // (int, int)vector -> String
-    string getStr(vector<pair<int, int>> p){
-        
-        sort(p.begin(), p.end(), mycmp);
-
-        string str = "";
-        int x = p[0].first;
-        int y = p[0].second;
-        for (pair<int, int> point : p){
-            str += (to_string(point.first-x) + " " + to_string(point.second-y));
-        }
-
-    return str;
-    }
-
-    // src-right-down & src-down-right must be same and taken as unique
-    string getUnique(vector<pair<int, int>> v){
-        
-        vector<string> sameIslands;
-        
-        int trans[4][4] = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
-        for(int i = 0; i < 4; i++){
-            vector<pair<int, int>> p1;
-            vector<pair<int, int>> p2;        
-            for(pair<int, int> p: v){
-                int x = p.first;
-                int y = p.second;
-                p1.push_back({x*trans[i][0], y*trans[i][1]});
-                p2.push_back({y*trans[i][0], x*trans[i][1]});
-            }
-            sameIslands.push_back(getStr(p1));
-            sameIslands.push_back(getStr(p2));
-        }
-        
-        sort(sameIslands.begin(), sameIslands.end(), mycmp);
-
-    return sameIslands[0];
+        int temp = find(par[x], par);
+        par[x] = temp;
+    return temp;
     }
     
-    void dfs(vector<vector<int>>& grid, int x, int y, vector<pair<int, int>>& v){
+    vector<int> numIslands2(int m, int n, vector<Point> &operators) {
         
-        int m = grid.size();
-        int n = grid[0].size();
+        vector<int> v;
+        vector<vector<int>> dirs = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
 
-        v.push_back({x, y});
-        grid[x][y] = 0;
-
-        int dx[4] = {0, -1, 1, 0};
-        int dy[4] = {-1, 0, 0, 1};
-        for(int i = 0; i < 4; i++){
-            int nr = x + dx[i];
-            int nc = y + dy[i];
-            if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc]==1){
-                dfs(grid, nr, nc, v);
+        vector<int> par(m*n, -1);
+        vector<int> rank(m*n);
+        int cnt = 0;
+        for(auto p : operators){
+            
+            int row = p.x;
+            int col = p.y;
+            int cellnum = row*n + col;
+            if(par[cellnum] != -1){     // handle duplicacy of point
+                v.push_back(cnt);
+                continue;
             }
-        }
-    }
+            par[cellnum] = cellnum;
+            rank[cellnum] = 1;
+            cnt++;
 
-    int numDistinctIslands2(vector<vector<int>> &grid){
-    
-        int m = grid.size();
-        int n = grid[0].size();
+            for(auto dx : dirs){
+                int rowdash = row + dx[0];
+                int coldash = col + dx[1];
+                int celldash = rowdash*n + coldash;
 
-        set<string> s;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == 1){   
-                    vector<pair<int, int>> v;    // v[x, y]
-                    dfs(grid, i, j, v);
-                    s.insert(getUnique(v));
+                if(rowdash < 0 || coldash < 0 || rowdash >= m || coldash >= n || par[celldash] == -1) continue;
+
+                // union (if neighbour is 1)
+                int x_rep = find(cellnum, par);
+                int y_rep = find(celldash, par);
+                if(x_rep != y_rep){
+                    if(rank[x_rep] > rank[y_rep]){
+                        par[y_rep] = x_rep;
+                    }
+                    else if(rank[x_rep] < rank[y_rep]){
+                        par[x_rep] = y_rep;
+                    }
+                    else{
+                        par[y_rep] = x_rep;
+                        rank[x_rep]++;
+                    }
+                    cnt--;
                 }
-            }
-        }
-        
-    return (int)s.size();
-    }
+            } // check neighbours ends
+        v.push_back(cnt);
+        } // iterating over list ends
+    return v;
+    } //function ends
 };
 
 
