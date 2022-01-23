77d52fa9b6675c4829436c53b33a8e89cc54e2e6
kunal_cpp <kunaliesc@gmail.com>
2022-01-23 17:02:49 +0530

    Update As far from land as possible.cpp

--- As far from land as possible.cpp
+++ As far from land as possible.cpp
@@ -1,2 +1,73 @@
 /*
-Problem Link: 
+Problem Link: https://leetcode.com/problems/as-far-from-land-as-possible/
+
+As Far from Land as Possible
+
+Given an n x n grid containing only values 0 and 1, where 0 represents water and 1 represents land, find a water cell such that 
+its distance to the nearest land cell is maximized, and return the distance. If no land or water exists in the grid, return -1.
+
+The distance used in this problem is the Manhattan distance: the distance between two cells (x0, y0) and (x1, y1) is |x0 - x1| + |y0 - y1|.
+
+Example 1:
+Input: grid = [[1,0,1],[0,0,0],[1,0,1]]
+Output: 2
+Explanation: The cell (1, 1) is as far as possible from all the land with distance 2.
+
+Example 2:
+Input: grid = [[1,0,0],[0,0,0],[0,0,0]]
+Output: 4
+Explanation: The cell (2, 2) is as far as possible from all the land with distance 4.
+ 
+ 
+Constraints:
+n == grid.length
+n == grid[i].length
+1 <= n <= 100
+grid[i][j] is 0 or 1
+*/
+
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        
+        vector<vector<int>> dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        queue<pair<int, int>> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    grid[i][j] = 0;
+                    q.push({i, j});
+                } else{
+                    grid[i][j] = -1;
+                } 
+            }
+        }
+    
+        if(q.size() == 0 || q.size() == m*n) return -1;
+        
+        int maxi = INT_MIN;
+        while(!q.empty()){
+            
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int rowdash = r + dirs[i][0];
+                int coldash = c + dirs[i][1];
+                
+                if(rowdash >= 0 && coldash >= 0 && rowdash < m && coldash < n && grid[rowdash][coldash] < 0){
+                    grid[rowdash][coldash] = grid[r][c] + 1;
+                    q.push({rowdash, coldash});
+                    maxi = max(maxi, grid[rowdash][coldash]);
+                }
+            }  
+        }
+    return maxi;
+    }
+};
+
+
